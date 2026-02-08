448053bfa65119d416cf9d7396dd6380fa531a3b
Michel <Michel@gmail.com>
2026-02-08 15:12:56 -0300

    criando estado de trocas de botões

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -4,10 +4,13 @@ export const Card = styled.div`
   background-color: #fcfcfc;
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
   padding: 16px;
+  margin-bottom: 32px;
+  border-radius: 16px;
 `
 export const Titulo = styled.h3`
   font-size: 18px;
   font-weight: bold;
+  margin-bottom: 16px;
 `
 
 export const Tag = styled.span`
@@ -15,6 +18,10 @@ export const Tag = styled.span`
   color: #fff;
   font-size: 10px;
   font-weight: 700;
+  background-color: #e1a32a;
+  border-radius: 8px;
+  margin-right: 16px;
+  display: inline-block;
 `
 
 export const Descricao = styled.textarea`
@@ -22,6 +29,13 @@ export const Descricao = styled.textarea`
   font-size: 14px;
   line-height: 24px;
   font-family: 'Roboto Mono', monospace;
+  display: block;
+  width: 100%;
+  margin-bottom: 16px;
+  margin-top: 16px;
+  resize: none;
+  border: none;
+  background-color: transparent;
 `
 
 export const BarraAcoes = styled.div`
@@ -37,4 +51,6 @@ export const Botao = styled.button`
   border: none;
   cursor: pointer;
   background-color: #2f3640;
+  border-radius: 8px;
+  margin-right: 8px;
 `
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,26 +1,43 @@
 import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    prioridade: 'importante',
+    status: 'Pendente',
+    descricao: 'importante para o meu crescimento como dev'
+  },
+  {
+    titulo: 'pagar a conta de luz',
+    prioridade: 'urgente',
+    status: 'concluida',
+    descricao: 'baixar fatura no gmail'
+  },
+  {
+    titulo: 'Ir para a academia',
+    prioridade: 'importante',
+    status: 'Pendente',
+    descricao: 'fazer exercícios para manter a saúde'
+  }
+]
 
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
-  </main>
+  </Container>
 )
 
 export default ListaDeTarefas

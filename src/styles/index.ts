96e2e51408ad6b64ec38d49e3acfbcf144752705
Michel <Michel@gmail.com>
2026-02-10 20:07:16 -0300

    atualizando as cores das tags

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -5,8 +5,10 @@ const EstiloGlobal = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto', sans-serif;}
-  `
+    list-style: none;
+    font-family: 'Roboto', sans-serif;
+  }
+`
 export const Container = createGlobalStyle`
    display:grid;
    grid-template-columns: 224px auto;
---
+++ src/styles/variaveis.ts
@@ -0,0 +1,6 @@
+export default {
+  vermelho: '#c23616',
+  amarelo: '#f0c11a',
+  amarelo2: '#e1a32a',
+  verde: '#44bd32'
+}
